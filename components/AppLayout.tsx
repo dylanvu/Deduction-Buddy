254b734badd3c43d3d85998dd495e71696081f40
Dylan Vu <dylanvu9@gmail.com>
2024-07-20 18:52:23 -0700

    fix background color

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -1,9 +1,10 @@
 import { View } from "react-native";
 import Footer from "@/components/Footer";
+import { Colors } from "@/constants/Colors";
 
 function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1, backgroundColor: Colors.white }}>
       <View>{children}</View>
       <Footer />
     </View>
