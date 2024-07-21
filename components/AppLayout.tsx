152f1c717934af2d8906829de9093ef6470f0f5a
Dylan Vu <dylanvu9@gmail.com>
2024-07-20 19:46:30 -0700

    move layout styling to styling object instead

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -1,14 +1,23 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import Footer from "@/components/Footer";
 import { Colors } from "@/constants/Colors";
 
 function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <View style={{ flex: 1, backgroundColor: Colors.white }}>
+    <View style={styles.layout}>
       <View>{children}</View>
       <Footer />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+    backgroundColor: Colors.white,
+    // paddingTop: 80,
+    // maxWidth: 300,
+  },
+});
 
 export default AppLayout;
